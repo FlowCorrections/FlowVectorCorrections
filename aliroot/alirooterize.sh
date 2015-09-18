ffba6c06d3b4fc37b46f9c89a5d2434e6f974054
Jacobus Onderwaater <jonderwaater@gmail.com>
2015-09-18 20:33:35 +0300

    script for aliroot

---
+++ aliroot/alirooterize.sh
@@ -0,0 +1,37 @@
+# this script prepares the FlowVectorCorrections package for insertion in AliROOT
+# it prepends all file and class names with Ali
+# $1 points to the source directory
+# $2 points to the target directory
+
+inputfolder=$1
+outputfolder=$2
+
+rsync -av $1/ $2
+
+listclasses="Manager Axes DataVector QnVector Cuts Steps Histograms Configuration Constants"
+
+for j in $listclasses; do
+  mv $outputfolder/QnCorrections${j}.cxx $outputfolder/AliQnCorrections${j}.cxx
+  mv $outputfolder/QnCorrections${j}.h $outputfolder/AliQnCorrections${j}.h
+done
+
+listC=`find $outputfolder/ -name '*.cxx'`
+listH=`find $outputfolder/ -name '*.h'`
+
+for k in $listC; do
+  replacenames "$k"
+done
+for k in $listH; do
+  replacenames "$k"
+done
+
+replacenames ()
+{
+  file=$1
+  for j in $listclasses; do
+    sed -i "s/QnCorrections${j}/AliQnCorrections${j}/g" $file
+  done
+  sed -i "s/QNCORRECTIONS/ALIQNCORRECTIONS/g" $file
+}
+
+
