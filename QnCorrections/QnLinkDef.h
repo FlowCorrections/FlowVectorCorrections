58248e433812a0e433decadacf988095b6c1cd08
Jacobus Onderwaater <jonderwaater@gmail.com>
2015-09-18 20:35:40 +0300

    update to cmake files

---
+++ QnCorrections/QnLinkDef.h
@@ -0,0 +1,25 @@
+#ifdef __CINT__
+
+/************************************************************************** 
+ * Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>    * 
+ * See cxx source for full Copyright notice                               * 
+ *************************************************************************/ 
+ 
+/* Copyright(c) 1998-2015, ALICE Experiment at CERN, All rights reserved. *
+* See cxx source for full Copyright notice */
+
+ 
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class QnCorrectionsDataVector+;
+#pragma link C++ class QnCorrectionsConfiguration+;
+#pragma link C++ class QnCorrectionsAxes+;
+#pragma link C++ class QnCorrectionsCuts+;
+#pragma link C++ class QnCorrectionsQnVector+;
+#pragma link C++ class QnCorrectionsHistograms+;
+#pragma link C++ class QnCorrectionsManager+;
+#pragma link C++ class QnCorrectionsSteps+;
+
+#endif
