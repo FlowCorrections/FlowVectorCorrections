97e31cade74c068fbee792739444c1f3668a018e
Víctor González <victor.gonzalez@cern.ch>
2016-01-17 23:21:00 +0100

    Incorporated logging levels
    The warning error and fatal levels have
    been added and so support for selecting
    the one desired.

--- QnCorrections/QnCorrectionsLog.cxx
+++ QnCorrections/QnCorrectionsLog.cxx
@@ -1,19 +1,58 @@
 #include <TString.h>
 #include "QnCorrectionsLog.h"
 
+Int_t nLoggingLevel = kError;
+
+void QnCorrectionsSetTracingLevel(UInt_t level) {
+  if (level < kError)
+    /* don't mask errors */
+    nLoggingLevel = level;
+}
+
+
 void QnCorrectionsPrintMessageHandler(UInt_t type, const char* message,
                           const char* module, const char* className,
                           const char* function, const char* file, Int_t line)
 {
 // print the given message
 
-  TString sLocation = Form("I-%s/%s::%s: (%s:%.0d)",module,className,function,file,line);
+  TString sLocation = Form("%s/%s::%s: (%s:%.0d)",module,className,function,file,line);
 
   if (message != NULL) {
-    Info((const char *) sLocation, "\n   %s", message);
+    if (!(type < nLoggingLevel)) {
+      switch (type) {
+      case kInfo:
+        Info(Form("I-%s", (const char *) sLocation), "\n   %s", message);
+        break;
+      case kWarning:
+        Warning(Form("W-%s", (const char *) sLocation), "\n   %s", message);
+        break;
+      case kError:
+        Error(Form("E-%s", (const char *) sLocation), "\n   %s", message);
+        break;
+      case kFatal:
+        Fatal(Form("FATAL-%s", (const char *) sLocation), "\n   %s", message);
+        break;
+      }
+    }
   }
   else {
-    Info((const char *) sLocation,"%s", " ");
+    if (!(type < nLoggingLevel)) {
+      switch (type) {
+      case kInfo:
+        Info(Form("I-%s", (const char *) sLocation), "\n   %s", " ");
+        break;
+      case kWarning:
+        Warning(Form("W-%s", (const char *) sLocation), "\n   %s", " ");
+        break;
+      case kError:
+        Error(Form("E-%s", (const char *) sLocation), "\n   %s", " ");
+        break;
+      case kFatal:
+        Fatal(Form("FATAL-%s", (const char *) sLocation), "\n   %s", " ");
+        break;
+      }
+    }
   }
 }
 
--- QnCorrections/QnCorrectionsLog.h
+++ QnCorrections/QnCorrectionsLog.h
@@ -1,6 +1,7 @@
 #ifndef QNCORRECTIONS_LOG_H
 #define QNCORRECTIONS_LOG_H
 
+#ifndef ALICEFRAMEWORK
 #include <TClass.h>
 #include <TError.h>
 
@@ -22,11 +23,18 @@
 extern void QnCorrectionsPrintMessageHandler(UInt_t type, const char* message,
                           const char* module, const char* className,
                           const char* function, const char* file, Int_t line);
+extern void QnCorrectionsSetTracingLevel(UInt_t level);
 
 #define QnCorrectionsMessage(lvl,message) do { \
       QnCorrectionsPrintMessageHandler(lvl, message, MODULENAME(), ClassName(), FUNCTIONNAME(), __FILE__, __LINE__);} while(false)
 
 
-# define QnCorrectionsInfo(message)               QnCorrectionsMessage(kInfo, message)
+#define QnCorrectionsInfo(message)               QnCorrectionsMessage(kInfo, message)
+#define QnCorrectionsWarning(message)            QnCorrectionsMessage(kWarning, message)
+#define QnCorrectionsError(message)              QnCorrectionsMessage(kError, message)
+#define QnCorrectionsFatal(message)              QnCorrectionsMessage(kFatal, message)
 
+#else /* ALICEFRAMEWORK */
+#include "AliLog.h"
+#endif /* ALICEFRAMEWORK */
 #endif /* QNCORRECTIONS_LOG_H */
