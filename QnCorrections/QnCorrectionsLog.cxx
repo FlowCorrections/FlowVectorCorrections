3d5796ea42e725b726704207239c01890912fcda
Víctor González <victor.gonzalez@cern.ch>
2016-01-27 20:26:08 +0100

    Documenting the logging function

--- QnCorrections/QnCorrectionsLog.cxx
+++ QnCorrections/QnCorrectionsLog.cxx
@@ -1,15 +1,67 @@
+/**************************************************************************************************
+ *                                                                                                *
+ * Package:       FlowVectorCorrections                                                           *
+ * Authors:       Jaap Onderwaater, GSI, jacobus.onderwaater@cern.ch                              *
+ *                Ilya Selyuzhenkov, GSI, ilya.selyuzhenkov@gmail.com                             *
+ *                Víctor González, UCM, victor.gonzalez@cern.ch                                   *
+ *                Contributors are mentioned in the code where appropriate.                       *
+ * Development:   2012-2016                                                                       *
+ *                                                                                                *
+ * This file is part of FlowVectorCorrections, a software package that corrects Q-vector          *
+ * measurements for effects of nonuniform detector acceptance. The corrections in this package    *
+ * are based on publication:                                                                      *
+ *                                                                                                *
+ *  [1] "Effects of non-uniform acceptance in anisotropic flow measurements"                      *
+ *  Ilya Selyuzhenkov and Sergei Voloshin                                                         *
+ *  Phys. Rev. C 77, 034904 (2008)                                                                *
+ *                                                                                                *
+ * The procedure proposed in [1] is extended with the following steps:                            *
+ * (*) alignment correction between subevents                                                     *
+ * (*) possibility to extract the twist and rescaling corrections                                 *
+ *      for the case of three detector subevents                                                  *
+ *      (currently limited to the case of two “hit-only” and one “tracking” detectors)            *
+ * (*) (optional) channel equalization                                                            *
+ * (*) flow vector width equalization                                                             *
+ *                                                                                                *
+ * FlowVectorCorrections is distributed under the terms of the GNU General Public License (GPL)   *
+ * (https://en.wikipedia.org/wiki/GNU_General_Public_License)                                     *
+ * either version 3 of the License, or (at your option) any later version.                        *
+ *                                                                                                *
+ **************************************************************************************************/
+/// \file QnCorrectionsLog.cxx
+/// \brief Implementation of the logging function support
+
 #include <TString.h>
 #include "QnCorrectionsLog.h"
 
-UInt_t nLoggingLevel = kError;
+UInt_t nLoggingLevel = kError; ///< the current logging level
 
+/// Sets the logging level
+/// Supported levels match ROOT logging levels currently
+/// Info, Warning, Error and Fatal. Fatal produces a runtime
+/// error that stops execution while the other provide a
+/// meaningful message output.
+///
+/// Error and Fatal levels cannot be masked
+///
+/// \param level the desired logging level
 void QnCorrectionsSetTracingLevel(UInt_t level) {
   if (level < kError)
     /* don't mask errors */
     nLoggingLevel = level;
 }
 
-
+/// Support function for printing logging messages. The message
+/// will be printed if its level is higher than the current logging level.
+/// All parameters are incorporated by the message macro
+///
+/// \param type the level of the logging message
+/// \param message additional message to incorporate
+/// \param module the module the source file belongs to
+/// \param className the name of the class the function belongs to
+/// \param function the name of the function where the message was raised
+/// \param file source file name
+/// \param line source file line
 void QnCorrectionsPrintMessageHandler(UInt_t type, const char* message,
                           const char* module, const char* className,
                           const char* function, const char* file, Int_t line)
--- QnCorrections/QnCorrectionsLog.h
+++ QnCorrections/QnCorrectionsLog.h
@@ -1,22 +1,37 @@
 #ifndef QNCORRECTIONS_LOG_H
 #define QNCORRECTIONS_LOG_H
 
+/***************************************************************************
+ * Package:       FlowVectorCorrections                                    *
+ * Authors:       Jaap Onderwaater, GSI, jacobus.onderwaater@cern.ch       *
+ *                Ilya Selyuzhenkov, GSI, ilya.selyuzhenkov@gmail.com      *
+ *                Víctor González, UCM, victor.gonzalez@cern.ch            *
+ *                Contributors are mentioned in the code where appropriate.*
+ * Development:   2012-2016                                                *
+ * See cxx source for GPL licence et. al.                                  *
+ ***************************************************************************/
+
+/// \file QnCorrectionsLog.h
+/// \brief Logging function support for the Q vector correction framework
+
 #ifndef ALICEFRAMEWORK
 #include <TClass.h>
 #include <TError.h>
 
 #ifdef _MODULE_
+/// Macro that extracts the module name
 # define MODULENAME() _MODULE_
 #else
+/// Macro that extracts the module name
 # define MODULENAME() "NoModule"
 #endif
 
 // function name macro
 #if defined(__GNUC__) || defined(__ICC) || defined(__ECC) || defined(__APPLE__)
+/// Macro that extracts the function name
 # define FUNCTIONNAME() __FUNCTION__
-// #elif defined(__HP_aCC) || defined(__alpha) || defined(__DECCXX)
-// #define FUNCTIONNAME() __FUNC__
 #else
+/// Macro that extracts the function name
 # define FUNCTIONNAME() "???"
 #endif
 
@@ -25,13 +40,21 @@ extern void QnCorrectionsPrintMessageHandler(UInt_t type, const char* message,
                           const char* function, const char* file, Int_t line);
 extern void QnCorrectionsSetTracingLevel(UInt_t level);
 
+/// Actual way to invoke the logging function. It is
+/// a macro that incorporates the additional information needed
+/// for locating the source code the message was raised.
+/// \param lvl level of the logging message
+/// \param message meaningful message to print
 #define QnCorrectionsMessage(lvl,message) do { \
       QnCorrectionsPrintMessageHandler(lvl, message, MODULENAME(), ClassName(), FUNCTIONNAME(), __FILE__, __LINE__);} while(false)
 
-
+/// User function for an Info message
 #define QnCorrectionsInfo(message)               QnCorrectionsMessage(kInfo, message)
+/// User function for a Warning message
 #define QnCorrectionsWarning(message)            QnCorrectionsMessage(kWarning, message)
+/// User function for an Error message
 #define QnCorrectionsError(message)              QnCorrectionsMessage(kError, message)
+/// User function for a Fatal message: stop execution
 #define QnCorrectionsFatal(message)              QnCorrectionsMessage(kFatal, message)
 
 #else /* ALICEFRAMEWORK */
