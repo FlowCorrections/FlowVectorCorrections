8d67c19aa1d6b18ad8916534635f9b2614d9aee0
Víctor González <victor.gonzalez@cern.ch>
2016-02-19 20:38:26 +0100

    Gain equalization incorporated.

---
+++ QnCorrections/QnCorrectionsInputGainEqualization.cxx
@@ -0,0 +1,160 @@
+/**************************************************************************************************
+ *                                                                                                *
+ * Package:       FlowVectorCorrections                                                           *
+ * Authors:       Jaap Onderwaater, GSI, jacobus.onderwaater@cern.ch                              *
+ *                Ilya Selyuzhenkov, GSI, ilya.selyuzhenkov@gmail.com                             *
+ *                Víctor González, UCM, victor.gonzalez@cern.ch                                   *
+ *                Contributors are mentioned in the code where appropriate.                       *
+ * Development:   2012-2016                                                                       *
+ *                                                                                                *
+ * This file is part of FlowVectorCorrections, a software package that corrects Q-vector          *
+ * measurements for effects of nonuniform detector acceptance. The corrections in this package    *
+ * are based on publication:                                                                      *
+ *                                                                                                *
+ *  [1] "Effects of non-uniform acceptance in anisotropic flow measurements"                      *
+ *  Ilya Selyuzhenkov and Sergei Voloshin                                                         *
+ *  Phys. Rev. C 77, 034904 (2008)                                                                *
+ *                                                                                                *
+ * The procedure proposed in [1] is extended with the following steps:                            *
+ * (*) alignment correction between subevents                                                     *
+ * (*) possibility to extract the twist and rescaling corrections                                 *
+ *      for the case of three detector subevents                                                  *
+ *      (currently limited to the case of two “hit-only” and one “tracking” detectors)            *
+ * (*) (optional) channel equalization                                                            *
+ * (*) flow vector width equalization                                                             *
+ *                                                                                                *
+ * FlowVectorCorrections is distributed under the terms of the GNU General Public License (GPL)   *
+ * (https://en.wikipedia.org/wiki/GNU_General_Public_License)                                     *
+ * either version 3 of the License, or (at your option) any later version.                        *
+ *                                                                                                *
+ **************************************************************************************************/
+
+/// \file QnCorrectionsInputGainEqualization.cxx
+/// \brief Implementation of procedures for gain equalization on input data.
+#include "QnCorrectionsEventClasses.h"
+#include "QnCorrectionsHistograms.h"
+#include "QnCorrectionsCorrectionSteps.h"
+#include "QnCorrectionsDetector.h"
+#include "QnCorrectionsInputGainEqualization.h"
+
+///< the minimum value that will be considered as meaningful for processing
+const Float_t  QnCorrectionsInputGainEqualization::fMinimumSignificantValue = 1E-6;
+///< the name of the correction step
+const char *QnCorrectionsInputGainEqualization::szCorrectionName = "Gain equalization";
+///< the key of the correction step for ordering purpose
+const char *QnCorrectionsInputGainEqualization::szKey = "CCCC";
+
+
+/// \cond CLASSIMP
+ClassImp(QnCorrectionsInputGainEqualization);
+/// \endcond
+
+/// Default constructor
+/// Passes to the base class the identity data for the Gain equalization correction step
+QnCorrectionsInputGainEqualization::QnCorrectionsInputGainEqualization() :
+    QnCorrectionsCorrectionOnInputData() {
+  fInputHistograms = NULL;
+  fCalibrationHistograms = NULL;
+  fEqualizationMethod = QEQUAL_noEqualization;
+}
+
+/// Default destructor
+/// Releases the memory taken
+QnCorrectionsInputGainEqualization::~QnCorrectionsInputGainEqualization() {
+  if (fInputHistograms != NULL)
+    delete fInputHistograms;
+  if (fCalibrationHistograms != NULL)
+    delete fCalibrationHistograms;
+}
+
+/// Attaches the needed input information to the correction step
+///
+/// Pure virtual function
+/// \param list list where the inputs should be found
+/// \return kTRUE if everything went OK
+Bool_t QnCorrectionsInputGainEqualization::AttachInput(TList *list) {
+  /* TODO: shall we adapt the list here */
+  if (fInputHistograms->AttachHistograms(list)) {
+    fState = QCORRSTEP_applyCollect;
+    return kTRUE;
+  }
+  return kFALSE;
+}
+/// Asks for support histograms creation
+///
+/// Allocates the histogram objects and creates the calibration histograms.
+/// \param list list where the histograms should be incorporated for its persistence
+/// \return kTRUE if everything went OK
+Bool_t QnCorrectionsInputGainEqualization::CreateSupportHistograms(TList *list) {
+  QnCorrectionsChannelDetectorConfiguration *ownerConfiguration =
+      static_cast<QnCorrectionsChannelDetectorConfiguration *>(fDetectorConfiguration);
+  fInputHistograms = new QnCorrectionsProfileChannelized("TODOin", "TODOin",
+      ownerConfiguration->GetEventClassVariablesSet(),ownerConfiguration->GetNoOfChannels());
+  fCalibrationHistograms = new QnCorrectionsProfileChannelized("TODOcal", "TODOcal",
+      ownerConfiguration->GetEventClassVariablesSet(),ownerConfiguration->GetNoOfChannels());
+  fCalibrationHistograms->CreateProfileHistograms(list,
+      ownerConfiguration->GetUsedChannelsMask(), ownerConfiguration->GetChannelsGroups());
+  return kTRUE;
+}
+/// Processes the correction step
+///
+/// Pure virtual function
+/// \return kTRUE if everything went OK
+Bool_t QnCorrectionsInputGainEqualization::Process(const Float_t *variableContainer) {
+  switch (fState) {
+  case QCORRSTEP_calibration:
+    /* collect the data needed to further produce equalization parameters */
+    for(Int_t ixData = 0; ixData < fDetectorConfiguration->GetInputDataBank()->GetEntriesFast(); ixData++){
+      QnCorrectionsChannelizedDataVector *dataVector =
+          static_cast<QnCorrectionsChannelizedDataVector *>(fDetectorConfiguration->GetInputDataBank()->At(ixData));
+      fCalibrationHistograms->Fill(variableContainer, dataVector->GetId(), dataVector->Weight());
+    }
+    return kFALSE;
+    break;
+  case QCORRSTEP_applyCollect:
+    /* collect the data needed to further produce equalization parameters */
+    for(Int_t ixData = 0; ixData < fDetectorConfiguration->GetInputDataBank()->GetEntriesFast(); ixData++){
+      QnCorrectionsChannelizedDataVector *dataVector =
+          static_cast<QnCorrectionsChannelizedDataVector *>(fDetectorConfiguration->GetInputDataBank()->At(ixData));
+      fCalibrationHistograms->Fill(variableContainer, dataVector->GetId(), dataVector->Weight());
+    }
+    /* and proceed to ... */
+  case QCORRSTEP_apply: /* apply the equalization */
+    /* store the equalized weights in the data vector bank according to equalization method */
+    switch (fEqualizationMethod) {
+    case QEQUAL_noEqualization:
+      for(Int_t ixData = 0; ixData < fDetectorConfiguration->GetInputDataBank()->GetEntriesFast(); ixData++){
+        QnCorrectionsChannelizedDataVector *dataVector =
+            static_cast<QnCorrectionsChannelizedDataVector *>(fDetectorConfiguration->GetInputDataBank()->At(ixData));
+        dataVector->SetEqualizedWeight(dataVector->Weight());
+      }
+      break;
+    case QEQUAL_averageEqualization:
+      for(Int_t ixData = 0; ixData < fDetectorConfiguration->GetInputDataBank()->GetEntriesFast(); ixData++){
+        QnCorrectionsChannelizedDataVector *dataVector =
+            static_cast<QnCorrectionsChannelizedDataVector *>(fDetectorConfiguration->GetInputDataBank()->At(ixData));
+        Float_t average = fInputHistograms->GetBinContent(fInputHistograms->GetBin(variableContainer, dataVector->GetId()));
+        Float_t width = fInputHistograms->GetBinError(fInputHistograms->GetBin(variableContainer, dataVector->GetId()));
+        if (fMinimumSignificantValue < average)
+          dataVector->SetEqualizedWeight(1.0 + (dataVector->Weight() - average) / width);
+        else
+          dataVector->SetEqualizedWeight(0.0);
+      }
+      break;
+    case QEQUAL_widthEqualization:
+      for(Int_t ixData = 0; ixData < fDetectorConfiguration->GetInputDataBank()->GetEntriesFast(); ixData++){
+        QnCorrectionsChannelizedDataVector *dataVector =
+            static_cast<QnCorrectionsChannelizedDataVector *>(fDetectorConfiguration->GetInputDataBank()->At(ixData));
+        Float_t average = fInputHistograms->GetBinContent(fInputHistograms->GetBin(variableContainer, dataVector->GetId()));
+        if (fMinimumSignificantValue < average)
+          dataVector->SetEqualizedWeight(dataVector->Weight() / average);
+        else
+          dataVector->SetEqualizedWeight(0.0);
+      }
+      break;
+    break;
+    }
+  }
+  return kTRUE;
+}
+
---
+++ QnCorrections/QnCorrectionsInputGainEqualization.h
@@ -0,0 +1,110 @@
+#ifndef QNCORRECTIONS_INPUTGAINEQUALIZATION_H
+#define QNCORRECTIONS_INPUTGAINEQUALIZATION_H
+
+/***************************************************************************
+ * Package:       FlowVectorCorrections                                    *
+ * Authors:       Jaap Onderwaater, GSI, jacobus.onderwaater@cern.ch       *
+ *                Ilya Selyuzhenkov, GSI, ilya.selyuzhenkov@gmail.com      *
+ *                Víctor González, UCM, victor.gonzalez@cern.ch            *
+ *                Contributors are mentioned in the code where appropriate.*
+ * Development:   2012-2016                                                *
+ * See cxx source for GPL licence et. al.                                  *
+ ***************************************************************************/
+
+/// \file QnCorrectionsInputGainEqualization.h
+/// \brief Definition of the class that implements gain equalization of individual channels.
+///
+/// Gain equalization is applied on raw data from the involved detector.
+/// Two procedures are implemented: average gain equalization and width equalization.
+///
+/// The average gain equalization is applied for the signal \f$ \mbox{M}_{c,i} \f$ of each detector
+/// channel \f$ c \f$ measured for event \f$ i \f$ according to:
+/// \f[
+///        \mbox{M}'_{c,i} = \frac{\mbox{M}_{c,i}}{\langle\mbox{M}_{c}\rangle}
+/// \f]
+/// where  \f$\langle\mbox{M}_{c}\rangle\f$ is an average over events in a given event class
+/// \f[
+///        \langle\mbox{M}_{c}\rangle = \frac{1}{\mbox{N}_{ev}} \Sigma_{i}^{\mbox{N}_{ev}} \mbox{M}_{c,i}
+/// \f]
+///
+/// The width equalization is applied for the signal \f$ \mbox{M}_{c,i} \f$ of each detector
+/// channel \f$ c \f$ measured for event \f$ i \f$ according to:
+/// \f[
+///     \mbox{M}'_{c,i} = \mbox{A} + \mbox{B} \frac{\mbox{M}_{c,i} - \langle\mbox{M}_{c}\rangle}
+///                               {\sigma_{{M}_{c}}}
+/// \f]
+/// with A and B are parameters that are the same for all channels and
+/// \f$\sigma_{{M}_{c}}\f$ is the standard deviation of the signals of the channel \f$c\f$
+/// for the considered event class.
+/// \f[
+///        \sigma_{{M}_{c}} = \sqrt{
+///          \frac{1}{\mbox{N}_{ev}} \Sigma_{i}^{\mbox{N}_{ev}} \mbox{M}^2_{c,i} -
+///          \frac{1}{\mbox{N}^2_{ev}} \left(\Sigma_{i}^{\mbox{N}_{ev}} \mbox{M}_{c,i}\right)^2}
+/// \f]
+///
+/// The gain equalization is only applied if the class instance
+/// is in the correction status. In order to be in that status the instance
+/// should have been able to get the proper calibration histograms that will
+/// provide the required averages per event class and channel.
+/// If the class instance is not in the correction status then, it is
+/// in the calibration one, collecting data for producing, once merged in a
+/// further phase, the calibration histograms.
+
+
+/// \typedef QnGainEqualizationMethod
+/// \brief The class of the id of the supported gain equalization methods
+///
+/// Actually it is not a class because the C++ level of implementation.
+/// But full protection will be reached when were possible declaring it
+/// as a class.
+///
+typedef enum {
+  QEQUAL_noEqualization,         ///< \f$ \mbox{M'} = \mbox{M}\f$
+  QEQUAL_averageEqualization,    ///< \f$ \mbox{M}' = \frac{\mbox{M}}{\langle\mbox{M}\rangle} \f$
+  QEQUAL_widthEqualization,      ///< \f$ \mbox{M}' = \mbox{A} + \mbox{B} \frac{\mbox{M} - \langle\mbox{M} \rangle}{\sigma_{{M}}} \f$
+} QnGainEqualizationMethod;
+
+
+
+class QnCorrectionsInputGainEqualization : public QnCorrectionsCorrectionOnInputData {
+public:
+  QnCorrectionsInputGainEqualization();
+  ~QnCorrectionsInputGainEqualization();
+
+  /// Stores the passed equalization method
+  /// \param method the desired equalization method
+  void SetEqualizationMethod(QnGainEqualizationMethod method)
+  { fEqualizationMethod = method; }
+
+  /// Attaches the needed input information to the correction step
+  ///
+  /// Pure virtual function
+  /// \param list list where the inputs should be found
+  /// \return kTRUE if everything went OK
+  virtual Bool_t AttachInput(TList *list);
+  /// Asks for support histograms creation
+  ///
+  /// Pure virtual function
+  /// \param list list where the histograms should be incorporated for its persistence
+  /// \return kTRUE if everything went OK
+  virtual Bool_t CreateSupportHistograms(TList *list);
+
+  virtual Bool_t Process(const Float_t *variableContainer);
+  /// Clean the correction to accept a new event
+  /// Does nothing for the time being
+  virtual void ClearCorrectionStep() {}
+
+private:
+  static const Float_t  fMinimumSignificantValue;     ///< the minimum value that will be considered as meaningful for processing
+  static const char *szCorrectionName;               ///< the name of the correction step
+  static const char *szKey;                          ///< the key of the correction step for ordering purpose
+  QnCorrectionsProfileChannelized *fInputHistograms; ///< the histogram with calibration information
+  QnCorrectionsProfileChannelized *fCalibrationHistograms; ///< the histogram for building calibration information
+  QnGainEqualizationMethod fEqualizationMethod; ///< the selected equalization method
+
+/// \cond CLASSIMP
+  ClassDef(QnCorrectionsInputGainEqualization, 1);
+/// \endcond
+};
+
+#endif // QNCORRECTIONS_INPUTGAINEQUALIZATION_H
