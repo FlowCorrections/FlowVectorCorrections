81db1943243eb260105cf04bb8783332bf812b90
victor-gonzalez <victor.gonzalez@cern.ch>
2016-06-25 00:36:22 +0200

    The apply on twist and rescale

--- QnCorrections/QnCorrectionsQnVectorTwistAndRescale.h
+++ QnCorrections/QnCorrectionsQnVectorTwistAndRescale.h
@@ -41,25 +41,25 @@
 ///     \Lambda ^{\pm}_{2n} = \frac{\langle Y_{2n} \rangle}{A^{\pm}_{2n}}
 /// \f]
 /// For the correlations method two additional subdetectors need to be configured. Let these two subdetectors be
-/// denominated \f$ A \f$ and \f$ C \f$ being \f$ B \f$ the current subdetector on which the twist and rescaling
+/// denominated \f$ B \f$ and \f$ C \f$ being \f$ A \f$ the current subdetector on which the twist and rescaling
 /// correction will be applied. Then
 ///
 /// \f[
-/// A^{B+}_{2n} = \frac{\sqrt{2 \langle X^{A}_{2n} X^{B}_{2n} \rangle}\langle X^{A}_{2n} X^{C}_{2n} \rangle}
-/// {\langle X^{A}_{2n} X^{C}_{2n} \rangle \langle X^{B}_{2n} X^{C}_{2n} \rangle + \langle X^{A}_{2n} Y^{C}_{2n} \rangle \langle X^{B}_{2n} Y^{C}_{2n} \rangle},
+/// A^{A+}_{2n} = \frac{\sqrt{2 \langle X^{A}_{2n} X^{C}_{2n} \rangle}\langle X^{A}_{2n} X^{B}_{2n} \rangle}
+/// {\sqrt{\langle X^{A}_{2n} X^{B}_{2n} \rangle \langle X^{B}_{2n} X^{C}_{2n} \rangle + \langle X^{A}_{2n} Y^{B}_{2n} \rangle \langle X^{B}_{2n} Y^{C}_{2n} \rangle}},
 /// \f]
 ///
 /// \f[
-/// A^{B-}_{2n} = \frac{\sqrt{2 \langle X^{A}_{2n} X^{B}_{2n} \rangle}\langle Y^{A}_{2n} Y^{C}_{2n} \rangle}
-/// {\langle X^{A}_{2n} X^{C}_{2n} \rangle \langle X^{B}_{2n} X^{C}_{2n} \rangle + \langle X^{A}_{2n} Y^{C}_{2n} \rangle \langle X^{B}_{2n} Y^{C}_{2n} \rangle},
+/// A^{A-}_{2n} = \frac{\sqrt{2 \langle X^{A}_{2n} X^{C}_{2n} \rangle}\langle Y^{A}_{2n} Y^{B}_{2n} \rangle}
+/// {\sqrt{\langle X^{A}_{2n} X^{B}_{2n} \rangle \langle X^{B}_{2n} X^{C}_{2n} \rangle + \langle X^{A}_{2n} Y^{B}_{2n} \rangle \langle X^{B}_{2n} Y^{C}_{2n} \rangle}},
 /// \f]
 ///
 /// \f[
-///  \Lambda^{B,+}_{2n} =  \frac{\langle X^{A}_{2n} Y^{B}_{2n} \rangle}{\langle X^{A}_{2n} X^{B}_{2n} \rangle}
+///  \Lambda^{A,+}_{2n} =  \frac{\langle X^{A}_{2n} Y^{B}_{2n} \rangle}{\langle X^{A}_{2n} X^{B}_{2n} \rangle}
 /// \f]
 /// and
 /// \f[
-///  \Lambda^{B,-}_{2n} =  \frac{\langle Y^{A}_{2n} X^{B}_{2n} \rangle}{\langle Y^{A}_{2n} Y^{B}_{2n} \rangle}
+///  \Lambda^{A,-}_{2n} =  \frac{\langle X^{A}_{2n} Y^{B}_{2n} \rangle}{\langle Y^{A}_{2n} Y^{B}_{2n} \rangle}
 /// \f]
 ///
 /// Twist and rescale are only applied if the class instance
@@ -70,13 +70,17 @@
 /// in the calibration one, collecting data for producing, once merged in a
 /// further phase, the correction histograms.
 ///
-/// Data collection for twist and rescale correction parameters building is
-/// performed on plain Qn vector and by using Q2n vector structure, i.e, the
-/// construction of the correction parameters is done by using the two multiple of
-/// the harmonics defined within the involved detector configuration
+/// Data collection for twist and rescale correction parameters building in the double harmonic method is
+/// performed on plain Qn vector and by using only the harmonics n such as its double 2n is within the range
+/// of the harmonics handled by the configuration.
 ///
-/// Correction is performed for all harmonics
-/// defined within the involved detector configuration
+/// Data collection for twist and rescale correction parameters building in the correlations method is
+/// performed on the highest corrected Qn vector on the involved detectors. Again correction is built only
+/// for the harmonics n which double 2n lies within the range handled by the different detector configurations.
+///
+/// Corrections are performed for the harmonics for which there are data collection support.
+
+/* harmonic multiplier */
 
 #include "QnCorrectionsCorrectionOnQvector.h"
 
@@ -122,8 +126,16 @@
 /// in the calibration one, collecting data for producing, once merged in a
 /// further phase, the correction histograms.
 ///
-/// Data collecting during calibration is performed for all harmonics
-/// defined within the involved detector configuration
+/// Data collection for twist and rescale correction parameters building in the double harmonic method is
+/// performed on plain Qn vector and by using only the harmonics n such as its double 2n is within the range
+/// of the harmonics handled by the configuration.
+///
+/// Data collection for twist and rescale correction parameters building in the correlations method is
+/// performed on the highest corrected Qn vector on the involved detectors. Again correction is built only
+/// for the harmonics n which double 2n lies within the range handled by the different detector configurations.
+///
+/// Correction are performed for the harmonics for which there are data collection support.
+///
 
 class QnCorrectionsHistogramSparse;
 
@@ -138,17 +150,21 @@ public:
    ///
    enum QnTwistAndRescaleMethod {
      TWRESCALE_doubleHarmonic,      ///< \f$ A^{\pm}_{2n} = 1 \pm \langle X_{2n} \rangle, \qquad \Lambda ^{\pm}_{2n} = \frac{\langle Y_{2n} \rangle}{A^{\pm}_{2n}} \f$
-     TWRESCALE_correlations,    ///< \f$ A^{B+}_{2n} = \frac{\sqrt{2 \langle X^{A}_{2n} X^{B}_{2n} \rangle}\langle X^{A}_{2n} X^{C}_{2n} \rangle}
-     /// {\langle X^{A}_{2n} X^{C}_{2n} \rangle \langle X^{B}_{2n} X^{C}_{2n} \rangle + \langle X^{A}_{2n} Y^{C}_{2n} \rangle \langle X^{B}_{2n} Y^{C}_{2n} \rangle}, \\
-     /// A^{B-}_{2n} = \frac{\sqrt{2 \langle X^{A}_{2n} X^{B}_{2n} \rangle}\langle Y^{A}_{2n} Y^{C}_{2n} \rangle}
-     /// {\langle X^{A}_{2n} X^{C}_{2n} \rangle \langle X^{B}_{2n} X^{C}_{2n} \rangle + \langle X^{A}_{2n} Y^{C}_{2n} \rangle \langle X^{B}_{2n} Y^{C}_{2n} \rangle}, \\
-     ///  \Lambda^{B,+}_{2n} =  \frac{\langle X^{A}_{2n} Y^{B}_{2n} \rangle}{\langle X^{A}_{2n} X^{B}_{2n} \rangle}, \quad
-     ///  \Lambda^{B,-}_{2n} =  \frac{\langle Y^{A}_{2n} X^{B}_{2n} \rangle}{\langle Y^{A}_{2n} Y^{B}_{2n} \rangle} \f$
+     TWRESCALE_correlations,    ///< \f$ A^{A+}_{2n} = \frac{\sqrt{2 \langle X^{A}_{2n} X^{C}_{2n} \rangle}\langle X^{A}_{2n} X^{B}_{2n} \rangle}
+     /// {\sqrt{\langle X^{A}_{2n} X^{B}_{2n} \rangle \langle X^{B}_{2n} X^{C}_{2n} \rangle + \langle X^{A}_{2n} Y^{B}_{2n} \rangle \langle X^{B}_{2n} Y^{C}_{2n} \rangle}}, \\
+     /// A^{A-}_{2n} = \frac{\sqrt{2 \langle X^{A}_{2n} X^{C}_{2n} \rangle}\langle Y^{A}_{2n} Y^{B}_{2n} \rangle}
+     /// {\sqrt{\langle X^{A}_{2n} X^{B}_{2n} \rangle \langle X^{B}_{2n} X^{C}_{2n} \rangle + \langle X^{A}_{2n} Y^{B}_{2n} \rangle \langle X^{B}_{2n} Y^{C}_{2n} \rangle}}, \\
+     ///  \Lambda^{A,+}_{2n} =  \frac{\langle X^{A}_{2n} Y^{B}_{2n} \rangle}{\langle X^{A}_{2n} X^{B}_{2n} \rangle}, \quad
+     ///  \Lambda^{A,-}_{2n} =  \frac{\langle X^{A}_{2n} Y^{B}_{2n} \rangle}{\langle Y^{A}_{2n} Y^{B}_{2n} \rangle} \f$
    };
 
    QnCorrectionsQnVectorTwistAndRescale();
   ~QnCorrectionsQnVectorTwistAndRescale();
 
+  /// Sets the method for extracting twist and rescale correction parameters
+  /// \param method the chosen method
+  void SetTwistAndRescaleMethod(QnTwistAndRescaleMethod method)
+  { fTwistAndRescaleMethod = method; }
   /// Controls if twist step shall be applied
   /// \param apply kTRUE for applying the twist step
   void SetApplyTwist(Bool_t apply)
@@ -157,7 +173,7 @@ public:
   /// \param apply kTRUE for applying the rescale step
   void SetApplyRescale(Bool_t apply)
   { fApplyRescale = apply; }
-  void SetReferenceConfigurationsForTwistAndRescale(const char *nameA, const char *nameC);
+  void SetReferenceConfigurationsForTwistAndRescale(const char *nameB, const char *nameC);
   /// Set the minimum number of entries for calibration histogram bin content validation
   /// \param nNoOfEntries the number of entries threshold
   void SetNoOfEntriesThreshold(Int_t nNoOfEntries) { fMinNoOfEntriesToValidate = nNoOfEntries; }
@@ -179,6 +195,7 @@ public:
 
 private:
   static const Int_t fDefaultMinNoOfEntries;         ///< the minimum number of entries for bin content validation
+  static const Double_t fMaxThreshold;               ///< highest absolute value for meaningful results
   static const char *szTwistCorrectionName;          ///< the name of the twist correction step
   static const char *szRescaleCorrectionName;        ///< the name of the rescale correction step
   static const char *szKey;                          ///< the key of the correction step for ordering purpose
@@ -189,17 +206,21 @@ private:
   static const char *szQANotValidatedHistogramName;  ///< the name and title for bin not validated QA histograms
   QnCorrectionsProfileComponents *fDoubleHarmonicInputHistograms; //!<! the histogram with calibration information for the double harmonic method
   QnCorrectionsProfileComponents *fDoubleHarmonicCalibrationHistograms; //!<! the histogram for building calibration information for the doubel harmonic method
-  QnCorrectionsProfileCorrelationComponentsHarmonics *fCorrelationsABInputHistograms; //!<! the histogram with calibration information for the correlations method (AB)
-  QnCorrectionsProfileCorrelationComponentsHarmonics *fCorrelationsBCInputHistograms; //!<! the histogram with calibration information for the correlations method (BC)
-  QnCorrectionsProfileCorrelationComponentsHarmonics *fCorrelationsCAInputHistograms; //!<! the histogram with calibration information for the correlations method (CA)
-  QnCorrectionsProfileCorrelationComponentsHarmonics *fCorrelationABCalibrationHistograms; //!<! the histogram for building calibration information for the correlations method (AB)
-  QnCorrectionsProfileCorrelationComponentsHarmonics *fCorrelationBCCalibrationHistograms; //!<! the histogram for building calibration information for the correlations method (BC)
-  QnCorrectionsProfileCorrelationComponentsHarmonics *fCorrelationCACalibrationHistograms; //!<! the histogram for building calibration information for the correlations method (CA)
+  QnCorrectionsProfile3DCorrelations *fCorrelationsInputHistograms; //!<! the histogram with calibration information for the correlations method
+  QnCorrectionsProfile3DCorrelations *fCorrelationsCalibrationHistograms; //!<! the histogram for building calibration information for the correlations method
   QnCorrectionsHistogramSparse *fQANotValidatedBin;    //!<! the histogram with non validated bin information
 
+  QnTwistAndRescaleMethod fTwistAndRescaleMethod;  ///< the chosen method for extracting twist and rescale correction parameters
   Bool_t fApplyTwist;              ///< apply the twist step
   Bool_t fApplyRescale;            ///< apply the rescale step
+  TString fBDetectorConfigurationName; ///< the name of the B detector configuration
+  QnCorrectionsDetectorConfigurationBase *fBDetectorConfiguration; ///< pointer to the B detector configuration
+  TString fCDetectorConfigurationName; ///< the name of the C detector configuration
+  QnCorrectionsDetectorConfigurationBase *fCDetectorConfiguration; ///< pointer to the C detector configuration
   Int_t fMinNoOfEntriesToValidate;              ///< number of entries for bin content validation threshold
+  QnCorrectionsQnVector *fTwistCorrectedQnVector;
+  QnCorrectionsQnVector *fRescaleCorrectedQnVector;
+  QnCorrectionsQnVector *fPlaneQnVector;
 
 /// \cond CLASSIMP
   ClassDef(QnCorrectionsQnVectorTwistAndRescale, 1);
